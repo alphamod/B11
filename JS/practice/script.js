52bf74e65903392dd040e2b600e4df68d64f8fef
Syed Salman <37833818+alphamod@users.noreply.github.com>
2023-11-05 00:14:46 +0000

    oijh

---
+++ JS/practice/script.js
@@ -0,0 +1,27 @@
+// only for browser.
+// alert("");
+
+var r = prompt("Please enter the time:");
+
+"Good Morning, Syed!"
+
+// switch (response) {
+//   case "Morning":
+//     alert("Good, Morning!");
+//     break;
+//   case "Evening":
+//     alert("Good, Evening!");
+//     break;
+//   case "noon":
+//     alert("Good Afternoon");
+//     break;
+//   default:
+//     alert("Good day!");
+// }
+
+alert(r);
+alert(typeof r);
+
+
+// get 2 prompts; 1 . name 2. timeof the day
+// greet accordingly with switch case & alert.
